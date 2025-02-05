71ef25b6a8aab5606a576016b0355e24e368fc58
HongliangLi <woodyli82@gmail.com>
2025-02-05 00:10:14 -0800

    add sample Flashcard test

--- src/components/Flashcard.vue
+++ src/components/Flashcard.vue
@@ -2,10 +2,24 @@
   <v-card>
     <v-card-title>{{ question }}</v-card-title>
     <v-card-text>
-      <v-text-field v-if="editable" v-model="answer" label="Answer" outlined></v-text-field>
+      <v-text-field
+        v-if="editable"
+        v-model="answer"
+        label="Answer"
+        outlined
+      ></v-text-field>
       <div v-else>{{ answer }}</div>
       <div>{{ note }}</div>
-      <v-img :src="img_url" v-if="img_url"></v-img>
+      <v-img :src="img_url" v-if="img_url" :width="300">
+        <template v-slot:placeholder>
+          <v-row align="center" class="fill-height ma-0" justify="center">
+            <v-progress-circular
+              color="grey-lighten-5"
+              indeterminate
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
     </v-card-text>
     <v-card-actions>
       <v-btn v-if="editable" @click="submitAnsffwer">Submit</v-btn>
@@ -15,7 +29,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue"
 
 const props = defineProps({
   question: String,
@@ -24,15 +38,15 @@ const props = defineProps({
   img_url: String,
   mode: {
     type: String,
-    default: 'readonly',
-    validator: value => ['readonly', 'editable', 'deletable'].includes(value)
-  }
-});
+    default: "readonly",
+    validator: (value) => ["readonly", "editable", "deletable"].includes(value),
+  },
+})
 
-const editable = ref(props.mode === 'editable');
-const deletable = ref(props.mode === 'deletable');
+const editable = ref(props.mode === "editable")
+const deletable = ref(props.mode === "deletable")
 
-const answer = ref(props.answer);
+const answer = ref(props.answer)
 
 function submitAnswer() {
   // handle answer submission
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Flashcard></Flashcard>
+  <Flashcard question="Hi" answer="y" mode="editable" note="a note" img_url="https://storage.googleapis.com/bucket-zoetrope-3372/83724701ca4394d65c7eccd37b1325dc.jpg"/>
 </template>
 
 <script setup>
