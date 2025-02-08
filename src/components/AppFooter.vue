3730b92828779d680005b00ff22cd61746ffdb11
h0ngliangli <woodyli1982@gmail.com>
2025-02-08 13:46:48 -0800

    调整footer样式 使其更清楚 https://hongliangli.atlassian.net/browse/QUICK-21?atlOrigin=eyJpIjoiN2UzY2MzMGM5YTY5NDRiZWExNDcyMDBlNjg2ZWI5NGUiLCJwIjoiaiJ9

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,16 @@
 <template>
-  <v-footer height="40" app style="background-color: transparent" class="pa-0">
+  <v-footer height="60" app style="background-color: transparent" class="pa-0">
     <v-sheet
       style="max-width: 800px"
       class="border w-100 h-100 ma-auto d-flex justify-center align-center"
     >
-      <router-link to="/" class="text-decoration-none" style="color: rgba(var(--v-theme-on-surface)">
-        <v-icon size="40q">mdi-home</v-icon>dd
+      <router-link
+        to="/"
+        class="text-decoration-none d-flex align-center flex-column"
+        style="color: rgba(var(--v-theme-on-surface)"
+      >
+        <v-icon size="40q">mdi-home</v-icon>
+        <span>Home</span>
       </router-link>
     </v-sheet>
   </v-footer>
